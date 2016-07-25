512f63dac1e05c0b0e61d3a036351721e14a3438
莫福见 <mofujian@danwenhaodemacbook-air.local>
2016-07-25 13:43:19 +0800

    Initial Commit

---
+++ BaoMiWanBiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaoMiWanBiao
+//
+//  Created by 莫福见 on 16/7/25.
+//  Copyright © 2016年 Manridy.Bobo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaoMiWanBiao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaoMiWanBiao
+//
+//  Created by 莫福见 on 16/7/25.
+//  Copyright © 2016年 Manridy.Bobo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
